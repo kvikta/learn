d0737927c87a4b8602de239153ac197e13aad051
kvikta <32357531+kvikta@users.noreply.github.com>
2023-07-20 18:15:14 +0300

    Handled Empty Names and input that are not actual numbers.

--- src/main/kotlin/numberGuessingGame.kt
+++ src/main/kotlin/numberGuessingGame.kt
@@ -1,29 +1,39 @@
 import kotlin.math.roundToInt
 
 fun main() {
-    println("Please Enter your Name:")
-    val gamerName: String? = readLine()
 
-    playNumberGame(gamerName.toString())
+    playNumberGame()
 }
 
-fun playNumberGame(playerName: String = "Player") {
-    println("Hello, $playerName!")
-    println("Guess a number between 10 and 20.")
-    val randomNumberHuman: Int? = readLine()?.toInt()
-    val randomNumberComputer = (Math.random() * 10) + 10
-    if (randomNumberHuman != null ) {
-        if (randomNumberHuman < 10 || randomNumberHuman > 20) {
-            println("Please enter a number between 10 and 20:")
-        } else {
-            if (randomNumberHuman == randomNumberComputer.roundToInt()) {
-                println("Congrats! Your guess is right! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
+fun playNumberGame(defaultPlayerName: String = "Player") {
+    println("Default player name is $defaultPlayerName")
+    println("Please Enter your Name:")
+    val gamerName: String? = readLine()
+    val playerName = gamerName.toString()
+    if (playerName.isNotEmpty()) {
+        println("Hello, $playerName!")
+        println("Guess a number between 10 and 20.")
+        try {
+            val randomNumberHuman: Int? = readLine()?.toInt()
+            val randomNumberComputer = (Math.random() * 10) + 10
+            if (randomNumberHuman != null ) {
+                if (randomNumberHuman < 10 || randomNumberHuman > 20) {
+                    println("This Number is NOT between 10 and 20!")
+                } else {
+                    if (randomNumberHuman == randomNumberComputer.roundToInt()) {
+                        println("Congrats! Your guess is right! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
+                    } else {
+                        println("Sorry! Your guess is wrong! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
+                    }
+                }
             } else {
-                println("Sorry! Your guess is wrong! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
+                println("You have not entered a number!")
             }
+        } catch (e: NumberFormatException) {
+            println("This is not a number!")
         }
     } else {
-        println("Please Enter an Actual Number between 10 & 20:")
+        println("This is not a valid Name:")
     }
 
 
