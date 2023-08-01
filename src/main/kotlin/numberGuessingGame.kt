0802bcaed01337577ff0863e665ad5241b4c4618
kvikta <32357531+kvikta@users.noreply.github.com>
2023-08-01 10:20:56 +0300

    Handled Empty Names and input that are not actual numbers.

--- src/main/kotlin/numberGuessingGame.kt
+++ src/main/kotlin/numberGuessingGame.kt
@@ -2,25 +2,26 @@ import kotlin.math.roundToInt
 
 fun main() {
 
-    playNumberGame()
+    playNumberGame()  //calling the function to play the game
 }
 
+//function defining how to play the game
 fun playNumberGame(defaultPlayerName: String = "Player") {
     println("Default player name is $defaultPlayerName")
     println("Please Enter your Name:")
-    val gamerName: String? = readLine()
+    val gamerName: String? = readLine()   //getting user input entering their name
     val playerName = gamerName.toString()
-    if (playerName.isNotEmpty()) {
+    if (playerName.isNotEmpty()) {  //checking that the user has not entered an empty name
         println("Hello, $playerName!")
         println("Guess a number between 10 and 20.")
-        try {
+        try {    // handling error and catching to throw exception in case entered value is not a number
             val randomNumberHuman: Int? = readLine()?.toInt()
             val randomNumberComputer = (Math.random() * 10) + 10
-            if (randomNumberHuman != null ) {
-                if (randomNumberHuman < 10 || randomNumberHuman > 20) {
+            if (randomNumberHuman != null ) {  //if statement to check that entered value is not null
+                if (randomNumberHuman < 10 || randomNumberHuman > 20) { //if statement that checks that number is between 10 and 20
                     println("This Number is NOT between 10 and 20!")
                 } else {
-                    if (randomNumberHuman == randomNumberComputer.roundToInt()) {
+                    if (randomNumberHuman == randomNumberComputer.roundToInt()) { //comparing entered number with generated number
                         println("Congrats! Your guess is right! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
                     } else {
                         println("Sorry! Your guess is wrong! \n Your guess: $randomNumberHuman \n Computer's number: ${randomNumberComputer.roundToInt()}")
