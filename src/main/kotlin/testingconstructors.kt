04ede1335bbf694809ddd79c778e762ac77bd05b
kvikta <32357531+kvikta@users.noreply.github.com>
2023-08-18 08:38:33 +0300

    confirmed calling another secondary constructor using this

---
+++ src/main/kotlin/testingconstructors.kt
@@ -0,0 +1,18 @@
+
+
+fun main() {
+ Add(1,2)
+}
+class Add{
+    //calling another secondary constructor using this
+    constructor(a:Int, b:Int): this(a,b,7){
+        val sumOfTwo = a + b
+        println("The sum of 2 is: $sumOfTwo")
+    }
+
+    //this executes first
+    constructor(a:Int, b:Int, c:Int) {
+        val sumOfThree = a + b + c
+        println("The sum of 3 is: $sumOfThree")
+    }
+}
